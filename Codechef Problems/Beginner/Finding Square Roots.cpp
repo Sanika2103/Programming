e594c6395a9a6e2f79d39a241eca3d3e821a2126
Sanika Patil <sanikapatil1021@gmail.com>
2021-08-08 10:38:44 +0530

    Add files via upload

---
+++ Codechef Problems/Beginner/Finding Square Roots.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+   int n,a;
+   cin>>n;
+   while(n--){
+       cin>>a;
+       int t = sqrt(a);
+       cout<<t<<endl;
+   }
+
+    return 0;
+}
