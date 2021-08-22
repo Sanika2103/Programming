a05e1905260e7f2fe4a38c54e014b9387bc2993b
Sanika Patil <sanikapatil1021@gmail.com>
2021-08-22 21:49:14 +0530

    Add files via upload

---
+++ Codechef Problems/Beginner/Sum OR Difference.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+    int n,a;
+    cin>>n>>a;
+    if(n>a){
+          cout<<n-a<<endl;
+      }
+      else{
+          cout<<n+a<<endl;
+      }
+	return 0;
+}
