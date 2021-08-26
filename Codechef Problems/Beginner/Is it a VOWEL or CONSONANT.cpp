dbc48ccb2f9c7d22fd428b09f0298e10058f6527
Sanika Patil <sanikapatil1021@gmail.com>
2021-08-26 09:44:40 +0530

    Add files via upload

---
+++ Codechef Problems/Beginner/Is it a VOWEL or CONSONANT.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char m;
+  cin >> m;
+
+  if (m == 'A' || m == 'E' || m == 'I' || m == 'O' || m == 'U')
+    {
+      cout << "Vowel";
+
+    }
+  else
+    cout << "Consonant";
+
+
+    return 0;
+}
