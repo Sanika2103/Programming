a5c752951e2a2ff6b4a45d7294a8e588ea74b583
Sanika Patil <sanikapatil1021@gmail.com>
2021-10-31 11:56:58 +0530

    Create InsertionSort.cpp

---
+++ Randoms/InsertionSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++){
+       cin>>a[i];
+   }
+   for(int i=1;i<n;i++){
+      int pivot=a[i];
+      int j=i-1;
+       while(a[j]>pivot && j>=0){
+             a[j+1]=a[j]; 
+             j--;
+           }
+       a[j+1]=pivot;
+   }
+  
+  for(int i=0;i<n;i++){
+      cout<<a[i]<<" ";
+  }
+    return 0;
+}
