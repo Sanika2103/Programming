f5707e4f5318547b60f5ef10ba063d985f6f1df5
Sanika Patil <sanikapatil1021@gmail.com>
2021-11-19 19:40:45 +0530

    Update binaryTree.cpp

--- Randoms/binaryTree.cpp
+++ Randoms/binaryTree.cpp
@@ -2,9 +2,21 @@
 
 using namespace std;
 class node{
-  
+  char c;
   node *left;
   node *right;
+  public:
+  node(){
+    c = NULL;
+    right = NULL;
+    left = NULL;
+  }
+  friend class tree;
+}
+class tree{
+    void insert(node *val);
+    void display();
+    void inorder(node *val);
 }
 int main()
 {
