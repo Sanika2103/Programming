8aca47dccdb804c6597a49b78f328da21ce74235
Sanika Patil <sanikapatil1021@gmail.com>
2021-11-21 13:49:14 +0530

    Update LinkedList.cpp

--- Randoms/LinkedList.cpp
+++ Randoms/LinkedList.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+
+using namespace std;
+class node{
+    int val;
+    node *next;
+    public:
+    node();
+    node(int key){
+        key = val;
+        next = NULL;
+    }
+    friend class Llist;
+    
+}
+class Llist{
+    void insertstr(int key);
+    void insertmid(int key);
+    void insertend(int key);
+    void delstr();
+    void delmid();
+    void delend();
+}
+
+void Llist ::insertstr(int key){
+    node *new1 = new node(key);
+    
+}
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
 
