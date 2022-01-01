9fe3f609bbee79b2f83415bd8d38c99b566da0de
Sanika Patil <sanikapatil1021@gmail.com>
2022-01-01 23:24:57 +0530

    Update SieveofEratosthenes.cpp

--- Randoms/SieveofEratosthenes.cpp
+++ Randoms/SieveofEratosthenes.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void primeSeive(int n) {
+    int prime[n] = {0};
+    for (int i = 2; i <= n; ++i) {
+        if (prime[i] == 0) {
+            for (int j = i * i; j <= n; j += i) {
+                prime[j] = 1;
+
+            }
+        }
+
+    }
+    for (int i = 2; i <= n; ++i) {
+        if (prime[i] == 0)  cout << i << " ";
+    } cout << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    primeSeive(n);
+    return 0;
+}
